41d1b0365af1ea15d178ac1f04318203244471e4
unknown <tanvirxyz11@gmail.com>
2026-08-01 02:11:14 +0800

    copy_btn call_btn & heart_button done

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
-const allh3 = document.getElementsByTagName("h3");
-for (const h3 of allh3) {
-  h3.style.minHeight = "3.5rem";
+// heart number increase secition
+
+const allThinHeartLogo = document.getElementsByClassName("thin-heart-logo");
+for (let i = 0; i < allThinHeartLogo.length; i++) {
+  allThinHeartLogo[i].addEventListener("click", function () {
+    let loveNumber = parseInt(document.getElementById("love-number").innerText);
+    let newLoveNumber = loveNumber + 1;
+    document.getElementById("love-number").innerText = newLoveNumber;
+  });
+}
+
+// copy number increase section
+const allCopyCountBtn = document.getElementsByClassName("copy-count-btn");
+for (let i = 0; i < allCopyCountBtn.length; i++) {
+  allCopyCountBtn[i].addEventListener("click", function () {
+    let copyNumber = parseInt(document.getElementById("copy-number").innerText);
+    let newCopyNumber = copyNumber + 1;
+    const allBtnText = document.getElementsByClassName("copy-btn-text");
+    const btnText = allBtnText[i].innerText;
+
+    navigator.clipboard.writeText(btnText);
+
+    document.getElementById("copy-number").innerText = newCopyNumber;
+
+    alert("copied");
+  });
+}
+
+const allCallBtn = document.getElementsByClassName("call-count-btn");
+
+for (let i = 0; i < allCallBtn.length; i++) {
+  allCallBtn[i].addEventListener("click", function () {
+    let coinNumber = parseInt(document.getElementById("coin-number").innerText);
+
+    const alertMsgNumber = document.getElementsByClassName("copy-btn-text");
+    const singleAlertMsg = alertMsgNumber[i].innerText;
+
+    const allh3 = document.getElementsByTagName("h3");
+    const singleH3 = allh3[i].innerText;
+
+    if (coinNumber > 20) {
+      let newCoinNumber = coinNumber - 20;
+      document.getElementById("coin-number").innerText = newCoinNumber;
+
+      alert(`calling to :${singleAlertMsg}
+${singleH3}`);
+    } else {
+      alert("Please You May Recharge");
+    }
+  });
 }
